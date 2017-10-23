a31df18dc9adc16879603f3919428b0a68735d31
Dan <cyberpwnn@users.noreply.github.com>
2017-10-23 17:16:22 -0400

    Monitor controller base

---
+++ src/main/java/com/volmit/react/controller/MonitorController.java
@@ -0,0 +1,24 @@
+package com.volmit.react.controller;
+
+import surge.control.Controller;
+
+public class MonitorController extends Controller
+{
+	@Override
+	public void start()
+	{
+
+	}
+
+	@Override
+	public void stop()
+	{
+
+	}
+
+	@Override
+	public void tick()
+	{
+
+	}
+}
