42c282878f53f12241664dd63371b47db2e4c74d
Dan <cyberpwnn@users.noreply.github.com>
2017-10-24 15:12:06 -0400

    Player manage players

--- src/main/java/com/volmit/react/controller/PlayerController.java
+++ src/main/java/com/volmit/react/controller/PlayerController.java
@@ -1,9 +1,12 @@
 package com.volmit.react.controller;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.volmit.react.api.ReactPlayer;
 
+import surge.Surge;
 import surge.collection.GList;
 import surge.control.Controller;
 
@@ -15,18 +18,32 @@ public class PlayerController extends Controller
 	public void start()
 	{
 		players = new GList<ReactPlayer>();
+		Surge.register(this);
 	}
 
 	@Override
 	public void stop()
 	{
-
+		Surge.unregister(this);
 	}
 
 	@Override
 	public void tick()
 	{
 
+	}
+
+	public boolean has(Player p)
+	{
+		for(ReactPlayer i : players)
+		{
+			if(i.getP().equals(p))
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	public ReactPlayer getPlayer(Player p)
@@ -48,5 +65,15 @@ public class PlayerController extends Controller
 	public GList<ReactPlayer> getPlayers()
 	{
 		return players;
+	}
+
+	@EventHandler
+	public void on(PlayerQuitEvent e)
+	{
+		if(has(e.getPlayer()))
+		{
+			getPlayer(e.getPlayer()).save();
+			players.remove(getPlayer(e.getPlayer()));
+		}
 	}
 }
