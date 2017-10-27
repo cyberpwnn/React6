779b1fed1da5d9a1edc384cea1222d20f978d163
Dan <cyberpwnn@users.noreply.github.com>
2017-10-27 15:30:46 -0400

    Compile change

--- compile/release.bat
+++ compile/release.bat
@@ -5,7 +5,7 @@ echo Copying Target to Compile
 echo F|xcopy /y /s /f /q "%1" "%2"
 cd compile
 echo Rebuilding dictionary
-java -jar -Xmx1g -Xms1m FT.jar -k:20000
+java -jar -Xmx1g -Xms1m FT.jar -k:5000
 echo Obfuscating...
 java -jar -Xmx2g proguard.jar @obfuscation.cfg -verbose
 echo Copying Output Resources
