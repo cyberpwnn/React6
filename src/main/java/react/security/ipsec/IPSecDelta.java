4e8364aae09598f4c11f414e1cdd2898eb16116a
cyberpwn <danielmillst@gmail.com>
2018-02-15 05:53:37 -0500

    rdx6

---
+++ src/main/java/react/security/ipsec/IPSecDelta.java
@@ -0,0 +1,42 @@
+package react.security.ipsec;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import surge.util.Anchor;
+
+@Anchor(-10)
+public class IPSecDelta extends Thread
+{
+	private String ip = null;
+
+	public IPSecDelta()
+	{
+
+	}
+
+	@Override
+	public void run()
+	{
+		try
+		{
+			URL url = new URL("http://checkip.amazonaws.com/");
+			InputStreamReader isr = new InputStreamReader(url.openStream());
+			BufferedReader bu = new BufferedReader(isr);
+			String i = bu.readLine();
+			bu.close();
+			ip = i;
+		}
+
+		catch(Exception e)
+		{
+
+		}
+	}
+
+	public String getIp()
+	{
+		return ip;
+	}
+}
---
+++ src/main/java/react/security/locks/SecurityLockDelta.java
@@ -0,0 +1,50 @@
+package react.security.locks;
+
+import java.io.File;
+
+import org.bukkit.event.Listener;
+
+import react.api.PluginSelfDeleter;
+import react.controller.SecurityController;
+import surge.Surge;
+import surge.util.Anchor;
+
+@Anchor(-7)
+public class SecurityLockDelta implements Listener
+{
+	public SecurityLockDelta()
+	{
+		try
+		{
+			if(!SecurityController.failures.isEmpty())
+			{
+				File f = Surge.getPluginJarFile();
+				new PluginSelfDeleter(Surge.getAmp().getPluginInstance(), f, new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						if(f.exists())
+						{
+							try
+							{
+								f.delete();
+								f.deleteOnExit();
+							}
+
+							catch(Throwable e)
+							{
+
+							}
+						}
+					}
+				}).start();
+			}
+		}
+
+		catch(Exception e)
+		{
+
+		}
+	}
+}
